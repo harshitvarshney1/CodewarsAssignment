04445bcb38f8d4c133a9965c8cd46de63ddeb8cd
harshitvarshney1 <72073964+harshitvarshney1@users.noreply.github.com>
2021-05-10 18:27:51 +0530

    Two Joggers

---
+++ Two Joggers/Two Joggers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Two_Joggers
+{
+    class Kata
+    {
+        public static Tuple<int, int> NbrOfLaps(int x, int y)
+        {
+            int lcm = 1, i;
+            int max = (x > y) ? x : y;
+            for (i = max; ; i += max)
+            {
+                if (i % x == 0 && i % y == 0)
+                {
+                    lcm = i;
+                    break;
+                }
+            }
+            int a = lcm / x;
+            int b = lcm / y;
+
+            return new Tuple<int, int>(a, b);
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.NbrOfLaps(3, 5));
+        }
+    }
+}
