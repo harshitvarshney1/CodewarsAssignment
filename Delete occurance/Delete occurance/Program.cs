efc14aa8c15b41fd1fe177c889715d7823e60d48
harshitvarshney1 <72073964+harshitvarshney1@users.noreply.github.com>
2021-05-10 20:06:01 +0530

    Delete Resource

---
+++ Delete occurance/Delete occurance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Delete_occurance
+{
+    class Kata
+    {
+        public static int[] DeleteNth(int[] arr, int x)
+        {
+            var res = new List<int>();
+
+            foreach (var n in arr)
+            {
+                if (res.Count(i => i == n) < x)
+                {
+                    res.Add(n);
+                }
+            }
+            res.ForEach(i => Console.WriteLine(i.ToString()));
+            return res.ToArray();
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata.DeleteNth(new int[] { 1, 1, 3, 3, 7, 2, 2, 2, 2 }, 3);
+        }
+    }
+}
