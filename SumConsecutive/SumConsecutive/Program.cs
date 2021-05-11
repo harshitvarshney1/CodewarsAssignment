4719427e5f099b7641fc787c1c908e87961d6537
harshitvarshney1 <72073964+harshitvarshney1@users.noreply.github.com>
2021-05-11 22:03:28 +0530

    Sum Consecutive

---
+++ SumConsecutive/SumConsecutive/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumConsecutive
+{
+    class Program
+    {     
+        static void Main(string[] args)
+        {
+            var sc = SumConsecutives(new List<int>() { 1, 1, 3, 4,4, 7, 2, 2, 4, 4 });
+            sc.ForEach(i => Console.WriteLine(i.ToString()));
+        }
+        public static List<int> SumConsecutives(List<int> s)
+        {
+            List<int> ret = new List<int>();
+            int last = s[0], sum = 0;
+            foreach (int i in s)
+            {
+                if (i == last) sum += last;
+                else
+                {
+                    ret.Add(sum);
+                    sum = last = i;
+                }
+            }
+            ret.Add(sum);
+            return ret;
+        }
+    }
+    
+}
